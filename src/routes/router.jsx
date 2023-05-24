cf601b423fe43a37143ef4ff01b53e716d0103e9
Jimmy Bustillo <jimmy.bustillo@outlook.com>
2023-05-24 16:45:41 -0600

    Añade función de action al componente de Nuevo Cliente

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom"
 import Layout from "../components/Layout"
-import NuevoCliente from "../pages/NuevoCliente"
+import NuevoCliente, {
+  action as nuevoClienteAction,
+} from "../pages/NuevoCliente"
 import Index, { loader as clienteLoader } from "../pages/Index"
 
 const router = createBrowserRouter([
@@ -16,6 +18,7 @@ const router = createBrowserRouter([
       {
         path: "/clientes/nuevo",
         element: <NuevoCliente />,
+        action: nuevoClienteAction,
       },
     ],
   },

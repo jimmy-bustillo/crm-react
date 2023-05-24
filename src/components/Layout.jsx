3f2fbd052389b4504693414ff3b12c2326a71391
Jimmy Bustillo <jimmy.bustillo@outlook.com>
2023-05-23 21:30:03 -0600

    Agrega Link para navegar entre paginas

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,17 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, Link, useLocation } from "react-router-dom"
 
 function Layout() {
+  const location = useLocation()
+
   return (
     <div className='md:flex md:min-h-screen'>
       <aside className='md:w-1/4 bg-blue-900 px-5 py-10'>
         <h2 className="text-center text-4xl font-black text-white">CRM - Clientes</h2>
+
+        <div className="mt-10">
+          <Link className={`${location.pathname === '/' ? 'text-blue-300' : 'text-white'} text-2xl block mt-2 py-3 hover:text-blue-300`} to='/'>Clientes</Link>
+          <Link className={`${location.pathname === '/clientes/nuevo' ? 'text-blue-300' : 'text-white'} text-2xl block mt-2 py-3 hover:text-blue-300`} to='/clientes/nuevo'>Nuevo Cliente</Link>
+        </div>
       </aside>
       <main className='md:w-3/4 p-10 md:h-screen overflow-scroll'>
         <Outlet/>

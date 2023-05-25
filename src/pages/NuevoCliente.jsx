2b30abb1e3968b2c5715be784105d2c9c247a061
Jimmy Bustillo <jimmy.bustillo@outlook.com>
2023-05-25 15:46:33 -0600

    Valida el formulario y mustra un mensaje de error si todos los campos no estan llenos

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,11 +1,27 @@
-import { useNavigate, Form } from "react-router-dom"
+import { useNavigate, Form, useActionData } from "react-router-dom"
 import Formulario from "../components/Formulario"
+import Error from "../components/Error"
 
-export function action() {
-  return { ok: console.log("Submit al formulario") }
+export async function action({ request }) {
+  const formData = await request.formData()
+
+  const datos = Object.fromEntries(formData)
+
+  // Validación
+  const errores = []
+  if (Object.values(datos).includes("")) {
+    errores.push("Todos los campos son obligatorios")
+  }
+
+  // Retornar datos si hay errores
+  if (Object.keys(errores).length) {
+    return errores
+  }
 }
 
 function NuevoCliente() {
+  const errores = useActionData()
+
   const navigate = useNavigate()
 
   return (
@@ -25,6 +41,9 @@ function NuevoCliente() {
       </div>
 
       <div className='bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-20'>
+        {errores?.length &&
+          errores.map((error, i) => <Error key={i}>{error}</Error>)}
+
         <Form method='post'>
           <Formulario />
 

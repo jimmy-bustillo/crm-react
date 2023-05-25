531f9313239b99426342a3ada844ded98df527c0
Jimmy Bustillo <jimmy.bustillo@outlook.com>
2023-05-24 21:14:26 -0600

    Agrega estilos y un action para enviar los datos del formulario

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,7 +1,41 @@
+import { useNavigate, Form } from "react-router-dom"
+import Formulario from "../components/Formulario"
+
+export function action() {
+  return { ok: console.log("Submit al formulario") }
+}
 
 function NuevoCliente() {
+  const navigate = useNavigate()
+
   return (
-    <div>Clientes</div>
+    <>
+      <h1 className='font-black text-4xl text-blue-900'>Nuevo Cliente</h1>
+      <p className='mt-3'>
+        Llena todos los campos para registrar un nuevo cliente
+      </p>
+
+      <div className='flex justify-end'>
+        <button
+          className='bg-blue-800 text-white px-3 py-1 font-bold uppercase rounded-md'
+          onClick={() => navigate(-1)}
+        >
+          Volver
+        </button>
+      </div>
+
+      <div className='bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-20'>
+        <Form method='post'>
+          <Formulario />
+
+          <input
+            type='submit'
+            className='mt-5 w-full bg-blue-800 p-3 uppercase font-bold text-white text-lg rounded-md'
+            value='Registrar Cliente'
+          />
+        </Form>
+      </div>
+    </>
   )
 }
 

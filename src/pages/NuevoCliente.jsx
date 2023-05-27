8f9c8c26d448bcdde18c8466c56fe51ae3f7df12
Jimmy Bustillo <jimmy.bustillo@outlook.com>
2023-05-26 21:17:32 -0600

    Muestra mensaje de error ytulizando un regex

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -7,10 +7,19 @@ export async function action({ request }) {
 
   const datos = Object.fromEntries(formData)
 
+  const email = formData.get("email")
+
   // Validación
   const errores = []
   if (Object.values(datos).includes("")) {
     errores.push("Todos los campos son obligatorios")
+  }
+
+  let regex = new RegExp(
+    "([!#-'*+/-9=?A-Z^-~-]+(.[!#-'*+/-9=?A-Z^-~-]+)*|\"([]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(.[!#-'*+/-9=?A-Z^-~-]+)*|[[\t -Z^-~]*])"
+  )
+  if (!regex.test(email)) {
+    errores.push("El email no es valido")
   }
 
   // Retornar datos si hay errores
@@ -44,7 +53,7 @@ function NuevoCliente() {
         {errores?.length &&
           errores.map((error, i) => <Error key={i}>{error}</Error>)}
 
-        <Form method='post'>
+        <Form method='post' noValidate>
           <Formulario />
 
           <input

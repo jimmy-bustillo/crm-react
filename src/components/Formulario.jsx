06cedf9c4983228517d3ccdf8f88b65bc0a4293b
Jimmy Bustillo <jimmy.bustillo@outlook.com>
2023-06-06 21:45:47 -0600

    Rellena los campos del formlario con la información del cliente

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -12,6 +12,7 @@ const Formulario = ({cliente}) => {
                   className="mt-2 block w-full p-3 bg-gray-50 rounded-md"
                   placeholder="Nombre del Cliente"
                   name="nombre"
+                  defaultValue={cliente?.nombre}
               />
           </div>
           <div className="mb-4">
@@ -25,6 +26,8 @@ const Formulario = ({cliente}) => {
                   className="mt-2 block w-full p-3 bg-gray-50 rounded-md"
                   placeholder="Empresa del Cliente"
                   name="empresa"
+                  defaultValue={cliente?.empresa}
+
               />
           </div>
 
@@ -39,6 +42,8 @@ const Formulario = ({cliente}) => {
                   className="mt-2 block w-full p-3 bg-gray-50 rounded-md"
                   placeholder="Email del Cliente"
                   name="email"
+                  defaultValue={cliente?.email}
+
               />
           </div>
 
@@ -53,6 +58,8 @@ const Formulario = ({cliente}) => {
                   className="mt-2 block w-full p-3 bg-gray-50 rounded-md"
                   placeholder="Teléfono del Cliente"
                   name="telefono"
+                  defaultValue={cliente?.telefono}
+
               />
           </div>
 
@@ -68,6 +75,8 @@ const Formulario = ({cliente}) => {
                   className="mt-2 block w-full p-3 bg-gray-50 h-40 align-self rounded-md"
                   placeholder="Notas del Cliente"
                   name="notas"
+                  defaultValue={cliente?.notas}
+
               />
           </div>
       </>

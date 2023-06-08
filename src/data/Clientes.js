d779fb4f6d5384d0e41e88785ed14c2dae5ccaec
Jimmy Bustillo <jimmy.bustillo@outlook.com>
2023-06-07 21:38:41 -0600

    Añade función para eliminar los clientes

--- src/data/Clientes.js
+++ src/data/Clientes.js
@@ -20,9 +20,7 @@ export async function agregarCliente(datos) {
       },
     })
     await respuesta.json()
-  } catch (error) {
-    console.log(error)
-  }
+  } catch (error) {}
 }
 
 export async function actualizarCliente(id, datos) {
@@ -35,8 +33,16 @@ export async function actualizarCliente(id, datos) {
       },
     })
     await respuesta.json()
-  } catch (error) {
-    console.log(error)
-  }
-
+  } catch (error) {}
 }
+
+export async function eliminarCliente(id) {
+  try {
+    const respuesta = await fetch(`${import.meta.env.VITE_API_URL}/${id}`, {
+      method: "DELETE",
+    })
+    await respuesta.json()
+  } catch (error) {}
+}
+
+
